31359ffbae34403e6ca9137b2ff6338860cb813a
Glorp <jonas.winje@gmail.com>
2020-08-11 23:04:58 +0200

    mlap

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 const bokstaver = require('./bokstaver');
+const pos = bokstaver.lagPosisjon;
 
-const brett = bokstaver.lagBrett("abcde");
+const brett = bokstaver.lagBrett("ernsasdf");
 console.log(brett);
-const brett2 = bokstaver.flytt(brett, bokstaver.lagPosisjon(0,0), bokstaver.lagPosisjon(0,1));
-const brett3 = bokstaver.flytt(brett2, bokstaver.lagPosisjon(2,2), bokstaver.lagPosisjon(2,1));
+const brett2 = bokstaver.flytt(brett, pos(0,0), pos(0,1));
+const brett3 = bokstaver.flytt(brett2, pos(2,2), pos(1,0));
 console.log(brett3);
 
 console.log(bokstaver.finnGrupper(brett3));
